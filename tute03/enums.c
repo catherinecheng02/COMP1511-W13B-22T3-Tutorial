d160b2b475573db132003b7d6a1f57179636747b
catherinecheng02 <z5310517@ad.unsw.edu.au>
2022-09-28 17:31:34 +1000

    Create enums.c

---
+++ tute03/enums.c
@@ -0,0 +1,15 @@
+// Enum demo
+// Written by Catherine Cheng 28-09-2022
+#include <stdio.h>
+
+enum opal_card_type {
+    ADULT,
+    STUDENT,
+    CONCESSION
+};
+
+int main(void) {
+    enum opal_card_type cath_card = STUDENT;
+
+    return 0;
+}
